09b9539ad278cd1421f16566958d9476804cb355
Peter Mitsis <peter.mitsis@gmail.com>
2018-06-11 21:54:08 -0400

    rtusb_io: Control reaches end of non-void routines
    Resolves warnings for a cleaner build.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@gmail.com>

--- common/rtusb_io.c
+++ common/rtusb_io.c
@@ -519,8 +519,9 @@ int write_reg(
 
 	if (ret) {
 		DBGPRINT(RT_DEBUG_ERROR, ("write reg fail\n"));
-		return;
 	}
+
+	return ret;
 }
 
 int read_reg(
@@ -551,6 +552,8 @@ int read_reg(
 
 	if (ret)
 		*value = 0xffffffff;
+
+	return ret;
 }
 
 /*
